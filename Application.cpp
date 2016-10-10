70777ce7c1694618b9a6cdacd233b66b1a7a2af0
hindrik <hindrik1997@hotmail.com>
2016-10-10 18:16:06 +0200

    Fixed little problem, i did not clear the redo stack when executing commands.

--- Application.cpp
+++ Application.cpp
@@ -39,8 +39,8 @@ Application::~Application() {
 
 bool Application::initGLFW() {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     return true;
@@ -558,8 +558,6 @@ void Application::saveToFile()
         line.append("\n");
         lines.append(line);
     }
-
-
 
     std::ofstream out("datafile2.txt");
     out << lines;
